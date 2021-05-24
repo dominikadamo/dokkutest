7129d094d1bc65d6c0f9ca99694dd91263ccd881
Dominik Adamowski <do1adamo@gmail.com>
2021-05-24 22:30:22 +0200

    Change App.js

--- packages/digitalweb/src/App.js
+++ packages/digitalweb/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Updated app js, testchange2
+         Updated app js, testchange3
         </p>
         <a
           className="App-link"
